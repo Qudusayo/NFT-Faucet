ad0e42aa48821639f17c073e2167620d34ea0197
Qudusayo <qqudusayo@gmail.com>
2022-06-04 20:01:21 +0100

    UI Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./assets/NFT_FAUCET.png";
+import styles from "./App.module.scss";
+import { useState } from "react";
+import console from "console-browserify";
 
 function App() {
+  const [unit, setUnit] = useState(1);
+
+  const setAmountHandler = (direction) => {
+    if (direction === "+" && unit < 10) {
+      setUnit(unit + 1);
+    }
+    else if (direction === "-" && unit > 1) {
+      setUnit(unit - 1);
+    }
+  };
+
+
+  const nftClaimHandler = (e) => {
+    e.preventDefault();
+    console.log(unit);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div>
+        <img src={Banner} className={styles.AppDisplay} alt="logo" />
+      </div>
+      <div>
+        <form className={styles.AppForm} onSubmit={nftClaimHandler}>
+          <h2>Get Some Test NFT</h2>
+          <input type={"text"} />
+          <div className={styles.controlBlock}>
+            <button type="button" onClick={() => setAmountHandler("-")}>-</button>
+            <input type={"number"} value={unit} disabled />
+            <button type="button" onClick={() => setAmountHandler("+")}>+</button>
+          </div>
+          <input type={"submit"} />
+        </form>
+      </div>
     </div>
   );
 }
